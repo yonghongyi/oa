668d2b6431540670214f577bd4fbe114d45f93cb
yonghongyi <44463956+yonghongyi@users.noreply.github.com>
2018-10-26 16:15:27 +0800

    add
    add a method

--- Test.java
+++ Test.java
@@ -3,4 +3,10 @@ class Test{
 
 		System.out.println("this is first test!");
 	}
+	@Test
+	public void testGetBookById(){
+		BookService bs=new BookServiceImpl();
+		Book book = bs.getBookById("1");
+		System.out.println(book);
+	}
 }
