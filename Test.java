2e97affdd3d7790a570eefc3d962019a1616a7b9
yonghongyi <xiang19951230@qq.com>
2018-10-26 16:16:25 +0800

    second upate

--- Test.java
+++ Test.java
@@ -3,4 +3,14 @@ class Test{
 
 		System.out.println("this is first test!");
 	}
+	@Test
+	public void testGetBooks(){
+		BookService bs=new BookServiceImpl();
+		List<Book> books = bs.getBooks();
+		Book book1 = bs.getBookById("1");
+		for (Book book : books) {
+			System.out.println(book);
+		}
+		System.out.println(book1);
+	}
 }
